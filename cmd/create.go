1806a8ca943236d00bae6177af2865f2cc92dd11
Andrej Benz <hello@benz.dev>
2022-04-24 17:04:17 +0200

    added template creation

--- cmd/create.go
+++ cmd/create.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"log"
 
+	"github.com/abenz1267/related/creation"
 	"github.com/spf13/cobra"
 )
 
@@ -16,12 +17,21 @@ var Template = &cobra.Command{ //nolint
 	Short: "create template for given file",
 	Args:  cobra.MinimumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		// bla
-		log.Println(args)
-		log.Println(cmd.Flags().GetBool("global"))
+		result, err := cmd.Flags().GetBool("result")
+		if err != nil {
+			log.Panic(err)
+		}
+
+		global, err := cmd.Flags().GetBool("global")
+		if err != nil {
+			log.Panic(err)
+		}
+
+		creation.Template(result, global, args[0], args[1])
 	},
 }
 
 func init() {
 	Template.Flags().BoolP("global", "g", false, "if set, generated file will be placed in user configuration folder")
+	Template.Flags().BoolP("result", "r", false, "if set, the path to the generated template will be printed back")
 }
---
+++ creation/template.go
@@ -0,0 +1,49 @@
+package creation
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+const (
+	PermFile   = 0o644
+	PermFolder = 0o755
+)
+
+func Template(result bool, global bool, src, dest string) error {
+	destFolder := filepath.Join(".related", "templates")
+
+	if global {
+		userConfig, err := os.UserConfigDir()
+		if err != nil {
+			return fmt.Errorf("couldn't get user config dir: %w", err)
+		}
+
+		destFolder = filepath.Join(userConfig, "related", "templates")
+	}
+
+	source, err := ioutil.ReadFile(src)
+	if err != nil {
+		return fmt.Errorf("couldn't read source file: %w", err)
+	}
+
+	dest = filepath.Join(destFolder, dest+".tmpl")
+
+	err = os.MkdirAll(filepath.Dir(dest), PermFolder)
+	if err != nil {
+		return fmt.Errorf("couldn't create destination folder(s): %w", err)
+	}
+
+	err = ioutil.WriteFile(dest, source, PermFile)
+	if err != nil {
+		return fmt.Errorf("couldn't write template file: %w", err)
+	}
+
+	if result {
+		fmt.Println(dest) //nolint
+	}
+
+	return nil
+}
